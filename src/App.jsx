091768616fc8a558e36b392cbaed54906e93dccf
Kim Hanz <nanaka3@hotmail.com>
2025-08-07 15:35:19 +0700

    first commit group-project-react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center w-[800px] p-8">
+      <h1 className="font-bold text-4xl mb-5">The Core Home</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, quod
+        repudiandae! Magnam vero a dicta quia, illum amet veniam aperiam dolor
+        harum quibusdam doloremque ratione quo sequi minima mollitia dolores
+        nesciunt perspiciatis eius. Sapiente alias accusamus asperiores, ea
+        voluptas et. Voluptatum, quas doloribus minus officia quis quam minima
+        quo architecto.
+      </p>
+    </div>
+  )
+}
+
+export default App
